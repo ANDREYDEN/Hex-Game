b4a26e2118f419905eacfa716079491f9b7ad866
ANDREYDEN <andrey_den@ukr.net>
2018-12-28 00:02:35 +0200

    Add files via upload

--- consts.py
+++ consts.py
@@ -1,6 +1,7 @@
-"""Library of all constants"""
+import pygame as pg
+from funcs import Point
 
-# Colour constants
+# constants
 DARKRED = (155, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -12,29 +13,24 @@ WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 LIGHTYELLOW = (255, 255, 51)
 ORANGE = (255, 128, 0)
-
-# numerical constants
 EPS = 0.0000000001
 FPS = 30
 MAX_BOARD_SIZE = 20
 MIN_BOARD_SIZE = 5
 
-########################SETUP######################
-# images
+# setup()
 TILE_IMG = 'tile.png'
 BG_IMG = 'bg.jpg'
 PAUSE_IMG = 'pause.png'
 BACK_IMG = 'back.png'
 UP_IMG = 'up-arrow.png'
 DOWN_IMG = 'down-arrow.png'
-
-# docs
 RULES = 'rules.txt'
 BACKGROUND_MUSIC = 'bg-music.wav'
 CLICK_SOUND = 'click.wav'
-
 # size of the window
 W = 600
 H = 600
 # size of the grid
 SIZE = 11
+moves = [Point(1, 0), Point(1, -1), Point(0, 1), Point(0, -1), Point(-1, 1), Point(-1, 0)]
